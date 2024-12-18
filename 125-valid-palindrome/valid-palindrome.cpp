58ed4a58cf2fff309bd2fff5fd1fe1068d6e2a4b
Jeevan-27 <141016628+Jeevan-27@users.noreply.github.com>
2024-12-18 07:59:18 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (19.33%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        stack<char> st;
-        stack<char> st2;
+        // stack<char> st;
+        // stack<char> st2;
 
-        for(int i=0;i<s.length();i++){
-            if(iswalnum(s[i])){
-                st.push(tolower(s[i])); 
-            }
+        // for(int i=0;i<s.length();i++){
+        //     if(iswalnum(s[i])){
+        //         st.push(tolower(s[i])); 
+        //     }
+        // }
+
+        // stack<char> temp=st;
+        // while(!st.empty()){
+        //     st2.push(st.top());
+        //     st.pop();
+        // }
+
+        // while(!temp.empty()){
+        //     if(st2.top()!=temp.top()){
+        //         return false;
+        //     }
+        //     st2.pop();
+        //     temp.pop();
+        // }
+
+        // return true;
+
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) left++;   // Skip non-alphanumeric
+            while (left < right && !isalnum(s[right])) right--; // Skip non-alphanumeric
+            if (tolower(s[left]) != tolower(s[right])) return false; // Compare
+            left++;
+            right--;
         }
-
-        stack<char> temp=st;
-        while(!st.empty()){
-            st2.push(st.top());
-            st.pop();
-        }
-
-        while(!temp.empty()){
-            if(st2.top()!=temp.top()){
-                return false;
-            }
-            st2.pop();
-            temp.pop();
-        }
-
         return true;
     }
 };
